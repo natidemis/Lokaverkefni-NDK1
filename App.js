4ef8b45475dc1a5e59f2a6ec79b12f1988cb86c0
unknown <natidemisket@gmail.com>
2021-09-28 15:34:10 +0000

    Count and store in local storage

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+
+
+function CounterButton({ title, onIncrement, color}) {
+  return <Button color = {color} title={title} onPress={onIncrement} />
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +19,52 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  const [count, setCount] = useState(0)
+  const [restore, setRestore] = useState(true)
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('@storage_Key')
+        if(value !== null) {
+          setCount(Number.parseInt(value))
+        }
+      } catch(e) {
+        console.log(e)
+      }
+    }
+    if(restore) {
+      getData()
+      setRestore(false)
+    }
+  }, [restore])
+  var btnColor = 'cyan'
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem('@storage_Key', (count + 1).toString())
+      btnColor = 'cyan'
+      console.log()
+    } catch (e) {
+      // saving error
+      setCount(count - 1)
+      btnColor = 'red'
+    } finally{
+      setCount(count+1)
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Count: {count}</Text>
+      <CounterButton
+        color = {btnColor}
+        title="Increase count"
+        onIncrement={function(){
+          storeData()
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
