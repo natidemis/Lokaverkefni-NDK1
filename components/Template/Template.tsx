cfd3c8b159b114925b70fd56007e22ca2a34d76c
natidemis <natidemisket@gmail.com>
2021-10-27 01:22:38 +0000

    ident

--- components/Template/Template.tsx
+++ components/Template/Template.tsx
@@ -24,41 +24,41 @@ export default class TemplateView extends React.Component<{templates: TTemplate[
     render(){ 
       const { modalVisible } = this.state;
       return (
-          <View style={templateStyle.container}>
-            <Modal
-              animationType="slide"
-              transparent={true}
-              visible={modalVisible}
-              onRequestClose={() => {
-                Alert.alert("Modal has been closed.");
-                this.setModalVisible(!modalVisible);
-              }}
-            >
-              <View style={templateModalStyle.centeredView}>
-                <View style={templateModalStyle.modalView}>
-                  <Text style={templateModalStyle.modalText}>Hello World!</Text>
-                  <Pressable
-                    style={[templateModalStyle.button, templateModalStyle.buttonClose]}
-                    onPress={() => this.setModalVisible(!modalVisible)}
-                  >
-                    <Text style={templateModalStyle.textStyle}>Hide Modal</Text>
-                  </Pressable>
-                </View>
+        <View style={templateStyle.container}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              this.setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={templateModalStyle.centeredView}>
+              <View style={templateModalStyle.modalView}>
+                <Text style={templateModalStyle.modalText}>Hello World!</Text>
+                <Pressable
+                  style={[templateModalStyle.button, templateModalStyle.buttonClose]}
+                  onPress={() => this.setModalVisible(!modalVisible)}
+                >
+                  <Text style={templateModalStyle.textStyle}>Hide Modal</Text>
+                </Pressable>
               </View>
-            </Modal>  
-            {this.props.templates.map((template,i) => { 
-            return (
-                <TouchableHighlight onPress={() => this.setModalVisible(true)}>
-                    <View style={templateStyle.template} key={i}>
-                        <Text style={styles.title}>{template.title}</Text>
-                        {template.exercises.map((exercise,j) => {
-                            return (<Text key={i+j+1} style={styles.text}>{exercise.title}</Text>)
-                        })}
-                    </View> 
-                </TouchableHighlight>
-              )
-            })}   
-          </View> 
+            </View>
+          </Modal>  
+          {this.props.templates.map((template,i) => { 
+          return (
+              <TouchableHighlight onPress={() => this.setModalVisible(true)}>
+                  <View style={templateStyle.template} key={i}>
+                      <Text style={styles.title}>{template.title}</Text>
+                      {template.exercises.map((exercise,j) => {
+                          return (<Text key={i+j+1} style={styles.text}>{exercise.title}</Text>)
+                      })}
+                  </View> 
+              </TouchableHighlight>
+            )
+          })}   
+        </View> 
     )}
 }
 
