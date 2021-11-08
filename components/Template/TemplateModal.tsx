8d05a5050ec8dc9bca997ddcfe62812924fd0ff3
unknown <natidemisket@gmail.com>
2021-11-08 14:53:03 +0000

    fann villuna med mapping

--- components/Template/TemplateModal.tsx
+++ components/Template/TemplateModal.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 import { Modal, Pressable, View, Text } from "react-native";
-import { TSession } from "../../data/types";
+import { TSession, TTemplate } from "../../data/types";
 import { templateModalStyle } from "../../Styles";
 
-export default function TemplateModalComponent(props) {
+export default function TemplateModalComponent(
+  {modalVisible, activeTemplate, onChange}: {modalVisible: boolean, activeTemplate: TTemplate, onChange: Function}) {
 
     return(
      <Modal
        animationType="slide"
        transparent={true}
-       visible={props.modalVisible}
+       visible={modalVisible}
        onRequestClose={() => {
-         props.onChange(!props.modalVisible)
+         onChange(!modalVisible)
        }}
      >
        <View style={templateModalStyle.centeredView}>
          <View style={templateModalStyle.modalView}>
-           <Text>{props.activeTemplate?.title}</Text>
-           {props.activeTemplate && props.activeTemplate?.map((exercise, i) => {
-              return(<Text key={i}> {exercise.sets.length} '×' {exercise.title}</Text>)
+           <Text>{activeTemplate?.title}</Text>
+           {activeTemplate && activeTemplate?.exercises.map((exercise, i) => {
+              return(<Text key={i}> {exercise.sets.length} × {exercise.title}</Text>)
             })}
             <View style={templateModalStyle.buttonsView}
               onLayout={(event) => {
@@ -28,15 +29,15 @@ export default function TemplateModalComponent(props) {
             >
               <Pressable
                 style={[templateModalStyle.button, templateModalStyle.buttonClose]}
-                onPress={() => props.onChange(!props.modalVisible)}
+                onPress={() => onChange(!modalVisible)}
                >
                 <Text style={templateModalStyle.textStyle}>Start</Text>
               </Pressable>
               <Pressable
                 style={[templateModalStyle.button, templateModalStyle.buttonClose]}
-                onPress={() => props.onChange(!props.modalVisible)}
+                onPress={() => onChange(!modalVisible)}
               >
-                <Text style={templateModalStyle.textStyle}>Start</Text>
+                <Text style={templateModalStyle.textStyle}>Close</Text>
               </Pressable>
             </View>
          </View>
