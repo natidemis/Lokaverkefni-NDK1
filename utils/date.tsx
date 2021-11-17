0f6c8dbaeff049f0be8a50a5b6116f685c14a41b
unknown <natidemisket@gmail.com>
2021-11-17 16:17:50 +0000

    date utils

---
+++ utils/date.tsx
@@ -0,0 +1,16 @@
+const monthNames : string[] = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+const dateObj : Date = new Date();
+const month : string = monthNames[dateObj.getMonth()];
+const day : string = String(dateObj.getDate()).padStart(2, '0');
+const year : number = dateObj.getFullYear();
+const output : string = month  + '\n'+ day  + ',' + year;
+
+function getDayName(dateStr : string, locale : string)
+{
+    var date = new Date(dateStr);
+    return date.toLocaleDateString(locale, { weekday: 'long' });        
+}
+
+var dateStr = output;
+var cd = getDayName(dateStr, "nl-NL");
